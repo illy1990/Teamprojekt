710c2e9b4bd79db80eb974d9018b876e1a6a13f5
illy1990 <jascha.hell@outlook.de>
2020-03-26 15:27:54 +0100

    Toilet fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,13 +6,8 @@ let i = 0;
 let TimeId1 = setInterval(() => {
     if (shotsleft < 1) {
         document.getElementById("flyingBird").style.display = "none";
-
     } else {
-
-
         let XAchse = Math.floor(Math.random() * 73);
-        console.log(XAchse)
-
         document.getElementById("flyingBird").style.display = "block";
         document.getElementById("flyingBird").style.position = "absolute";
         document.getElementById("flyingBird").style.top = XAchse + "vh";
@@ -35,31 +30,73 @@ let TimeId1 = setInterval(() => {
     }
 }, 1000)
 
-
-
-
 document.getElementById("flyingBird").addEventListener("click", () => {
     if (shotsleft > 0) {
         PunktestandNeu = PunktestandNeu + 100;
         shotsleft = shotsleft - 1;
         document.getElementById("Ausgabe").innerHTML = PunktestandNeu;
         document.getElementById("flyingBird").style.display = "none"
-        document.getElementById("Restschusse").innerHTML = shotsleft;
-
+        ToilettenpapierEinblenden(shotsleft)
+        // document.getElementById("Restschusse").innerHTML = shotsleft;
     } else {
         document.getElementById("flyingBird").style.display = "none"
         shotsleft = 0;
-        document.getElementById("Restschusse").innerHTML = shotsleft;
+        ToilettenpapierEinblenden(shotsleft)
+
+        // document.getElementById("Restschusse").innerHTML = shotsleft;
     }
 })
+
+// document.getElementById("daneben").addEventListener("click", () => {
+//     if (shotsleft > 0) {
+//         shotsleft = shotsleft - 1;
+//         document.getElementById("Restschusse").innerHTML = shotsleft;
+//     } else {
+//         document.getElementById("flyingBird").style.display = "none"
+//         shotsleft = 0;
+//         document.getElementById("Restschusse").innerHTML = shotsleft;
+//     }
+// })
 
 document.getElementById("daneben").addEventListener("click", () => {
     if (shotsleft > 0) {
         shotsleft = shotsleft - 1;
-        document.getElementById("Restschusse").innerHTML = shotsleft;
+        ToilettenpapierEinblenden(shotsleft)
+
+        // document.getElementById("Restschusse").innerHTML = shotsleft;
     } else {
         document.getElementById("flyingBird").style.display = "none"
         shotsleft = 0;
-        document.getElementById("Restschusse").innerHTML = shotsleft;
+        ToilettenpapierEinblenden(shotsleft)
+
+        // document.getElementById("Restschusse").innerHTML = shotsleft;
     }
 })
+
+
+function ToilettenpapierEinblenden(shotsleft) {
+    if (shotsleft <= 9) {
+        document.getElementById("toilettenpapier10").style.display = "none";
+    } if (shotsleft <= 8) {
+        document.getElementById("toilettenpapier9").style.display = "none";
+    } if (shotsleft <= 7) {
+        document.getElementById("toilettenpapier8").style.display = "none";
+    } if (shotsleft <= 6) {
+        document.getElementById("toilettenpapier7").style.display = "none";
+    } if (shotsleft <= 5) {
+        document.getElementById("toilettenpapier6").style.display = "none";
+    } if (shotsleft <= 4) {
+        document.getElementById("toilettenpapier5").style.display = "none";
+    } if (shotsleft <= 3) {
+        document.getElementById("toilettenpapier4").style.display = "none";
+    } if (shotsleft <= 2) {
+        document.getElementById("toilettenpapier3").style.display = "none";
+    } if (shotsleft <= 1) {
+        document.getElementById("toilettenpapier2").style.display = "none";
+    } if (shotsleft <= 0) {
+        document.getElementById("toilettenpapier1").style.display = "none";
+    }
+    return shotsleft;
+}
+
+
